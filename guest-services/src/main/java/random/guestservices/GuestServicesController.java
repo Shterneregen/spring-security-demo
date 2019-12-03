94b9865c75f590838ed68ad5f21eb890790ba98d
shterneregen <shterneregen@gmail.com>
2019-12-03 16:44:10 +0300

    init commit

---
+++ guest-services/src/main/java/random/guestservices/GuestServicesController.java
@@ -0,0 +1,77 @@
+package random.guestservices;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/guests")
+public class GuestServicesController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GuestServicesController.class);
+
+    private final GuestRepository repository;
+
+    public GuestServicesController(GuestRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Guest> getAllGuests() {
+        return new ArrayList<>(repository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Guest> addGuest(@RequestBody GuestModel model) {
+        Guest guest = repository.save(model.translateModelToGuest());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(guest.getId()).toUri();
+        return ResponseEntity.created(location).body(guest);
+    }
+
+    @GetMapping("/{id}")
+    public Guest getGuest(@PathVariable Long id) {
+        Optional<Guest> guest = repository.findById(id);
+        if (guest.isPresent()) {
+            return guest.get();
+        }
+        throw new GuestNotFoundException("Guest not found with id: " + id);
+    }
+
+    @PutMapping("/{id}")
+    public Guest updateGuest(@PathVariable Long id, @RequestBody GuestModel model) {
+        Optional<Guest> existing = repository.findById(id);
+        if (!existing.isPresent()) {
+            throw new GuestNotFoundException("Guest not found with id: " + id);
+        }
+        Guest guest = model.translateModelToGuest();
+        guest.setId(id);
+        return repository.save(guest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.RESET_CONTENT)
+    public void deleteGuest(@PathVariable Long id) {
+        Optional<Guest> existing = repository.findById(id);
+        if (!existing.isPresent()) {
+            throw new GuestNotFoundException("Guest not found with id: " + id);
+        }
+        repository.deleteById(id);
+    }
+}
