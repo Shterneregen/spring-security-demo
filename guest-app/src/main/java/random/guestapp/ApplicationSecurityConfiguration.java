5002e6aa47f173648b7da895c2c50b126545cb8b
shterneregen <shterneregen@gmail.com>
2019-12-03 17:10:27 +0300

    in-memory auth

--- guest-app/src/main/java/random/guestapp/ApplicationSecurityConfiguration.java
+++ guest-app/src/main/java/random/guestapp/ApplicationSecurityConfiguration.java
@@ -1,9 +1,17 @@
 package random.guestapp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +26,15 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    // never ever use it on production
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("admin").password("psw").roles("USER", "ADMIN").build());
+        users.add(User.withDefaultPasswordEncoder().username("user1").password("user1").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
     }
 }
