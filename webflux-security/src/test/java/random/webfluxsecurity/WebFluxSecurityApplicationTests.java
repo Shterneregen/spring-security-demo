b4a1f1bf9edd7c0cc3c8b5ecbe56e528113b722f
shterneregen <shterneregen@gmail.com>
2019-12-13 15:57:13 +0300

    WebFlux without security

---
+++ webflux-security/src/test/java/random/webfluxsecurity/WebFluxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package random.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
